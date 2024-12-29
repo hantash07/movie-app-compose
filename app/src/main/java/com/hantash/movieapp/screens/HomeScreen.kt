deec048f3e634ce1338c525fa78911ff59665bf4
Hantash07 <hantashnadeem@gmail.com>
2024-12-29 16:15:07 +0500

    Improvements on Movie Row UI

--- app/src/main/java/com/hantash/movieapp/screens/HomeScreen.kt
+++ app/src/main/java/com/hantash/movieapp/screens/HomeScreen.kt
@@ -1,23 +1,9 @@
 package com.hantash.movieapp.screens
 
-import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -25,13 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.hantash.movieapp.model.getMovies
 import com.hantash.movieapp.navigation.EnumScreens
+import com.hantash.movieapp.widget.MovieRow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -54,18 +39,7 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun MainContent(modifier: Modifier, navController: NavController) {
-    val movieList = listOf(
-        "Movie Name 1",
-        "Movie Name 2",
-        "Movie Name 3",
-        "Movie Name 4",
-        "Movie Name 5",
-        "Movie Name 6",
-        "Movie Name 7",
-        "Movie Name 8",
-        "Movie Name 9",
-        "Movie Name 10",
-    )
+    val movieList = getMovies()
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
@@ -73,43 +47,9 @@ fun MainContent(modifier: Modifier, navController: NavController) {
         LazyColumn {
             items(items = movieList) { movie ->
                 MovieRow(movie, onItemClick = {
-                    navController.navigate(route = EnumScreens.DetailScreen.name + "/$movie")
+                    navController.navigate(route = EnumScreens.DetailScreen.name + "/${movie.title}")
                 })
             }
-        }
-    }
-}
-
-@Composable
-fun MovieRow(movie: String, onItemClick: () -> Unit) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .height(120.dp),
-        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
-        onClick = onItemClick,
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Surface(
-                modifier = Modifier
-                    .padding(12.dp)
-                    .size(80.dp),
-                shape = RectangleShape,
-            ) {
-                Icon(
-                    imageVector = Icons.Default.AccountBox,
-                    contentDescription = "Movie Image"
-                )
-            }
-            Text(text = movie)
         }
     }
 }
---
+++ app/src/main/java/com/hantash/movieapp/widget/Widget.kt
@@ -0,0 +1,143 @@
+package com.hantash.movieapp.widget
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
+import com.hantash.movieapp.model.Movie
+import com.hantash.movieapp.model.getMovies
+
+@Preview
+@Composable
+fun MovieRow(movie: Movie = getMovies().first(), onItemClick: () -> Unit = {}) {
+    val isExpanded = remember {
+        mutableStateOf(value = false)
+    }
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+//            .height(120.dp),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.background),
+        onClick = onItemClick,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(80.dp),
+                shape = RectangleShape,
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(
+                        model = movie.images.first()
+                    ),
+                    contentDescription = "Movie Image",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+//                Icon(
+//                    imageVector = Icons.Default.AccountBox,
+//                    contentDescription = "Movie Image"
+//                )
+//                AsyncImage(
+//                    model = "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMwNTg5MzMwMV5BMl5BanBnXkFtZTcwMzA2NTIyMw@@._V1_SX1777_CR0,0,1777,937_AL_.jpg",
+//                    contentDescription = "Movie Image"
+//                )
+            }
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = "Genre: ${movie.genre}",
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    text = "Ratings: ${movie.rating}",
+                    style = MaterialTheme.typography.bodySmall
+                )
+
+                Row(
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .clickable {
+                            isExpanded.value = !isExpanded.value
+                        },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Text(
+                        text = "Mode Detail",
+                        style = MaterialTheme.typography.labelSmall
+                    )
+                    Icon(
+                        imageVector = if (isExpanded.value) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                        contentDescription = "More Arrow Up/Down",
+                    )
+                }
+
+                AnimatedVisibility(
+                    visible = isExpanded.value
+                ) {
+                    Column {
+                        HorizontalDivider(modifier = Modifier.padding(4.dp))
+                        Text(
+                            text = "Director: ${movie.director}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        Text(
+                            text = "Actors: ${movie.actors}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                        Text(
+                            text = "Plot: ${movie.plot}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
